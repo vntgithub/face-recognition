2c1168ba2147531bd8a13892701d1d249e474432
Vo Nhat Trieu <vntrieu25879@gmail.com>
2021-04-27 12:23:27 +0700

    validate data form sigup done

--- src/pages/signup.page.js
+++ src/pages/signup.page.js
@@ -15,7 +15,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { MicNone } from '@material-ui/icons';
+import Alert from '@material-ui/lab/Alert';
+
+import './style/style.css';
+
 
 function Copyright() {
   return (
@@ -60,7 +63,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3),
     minWidth: 300,
   },
-  hiddenIuputImg: {
+  hidden: {
     display: 'none'
   },
   marginLeft: {
@@ -68,7 +71,7 @@ const useStyles = makeStyles((theme) => ({
   },
   mediumWidth: {
     minWidth: 150
-  }
+  }, 
 }));
 
 
@@ -76,6 +79,19 @@ export default function SignIn() {
   const classes = useStyles();
   const [position, setPosition] = React.useState('');
   const [open, setOpen] = React.useState(false);
+  const [image, setImage] = React.useState(null);
+  const [srcImage, setSrcImage] = React.useState('');
+  const [err, setErr] = React.useState({
+    name: false,
+    code: false,
+    username: false,
+    password: false,
+    passwordConfirm: false,
+    position: false,
+    image: false
+  });
+  const regexCode = /^[a-z]{2}\d{3,}$/;
+  const regexPassword = /[a-zA-z0-9]{8}/;
 
   const handleChange = (event) => {
     setPosition(event.target.value);
@@ -89,8 +105,82 @@ export default function SignIn() {
     setOpen(true);
   };
   const handleInput = () => {
-    document.getElementById('inputImg').click();
+    document.getElementById('image').click();
   }
+  const getImage = (e) => {
+    const file = e.target.files;
+    const imageData  = new FormData();
+    imageData.append('file', file[0]);
+    imageData.append('upload_preset', 'usersimage');
+    setImage(imageData);
+    //Review
+    let reader = new FileReader();
+    reader.onload = function (e) {
+            setSrcImage(e.target.result);
+        };
+
+    reader.readAsDataURL(file[0]);
+}
+  const vadlidateData = () => {
+    let check = true;
+    let newErr = {...err};
+    const name = document.getElementById('name').value;
+    const code =document.getElementById('code').value;
+    const username =document.getElementById('username').value;
+    const password =document.getElementById('password').value;
+    const passwordConfirm =document.getElementById('passwordConfirm').value;
+    if(name === ''){
+      check &= false;
+      newErr.name = true;
+    }else{
+      newErr.name = false;
+    }
+      
+    if(code === '' || !regexCode.test(code)){
+      check &= false;
+      newErr.code = true;
+    }else{
+      newErr.code = false;
+
+    }
+    if(username === '' || !regexPassword.test(username)){
+      check &= false;
+      newErr.username = true;
+    }else{
+      newErr.username = false;
+    }
+    if(password === '' || !regexPassword.test(password)){
+      check &= false;
+      newErr.password = true;
+    }else{
+      newErr.password = false;
+    }
+    if(passwordConfirm === '' || password !== passwordConfirm){
+      check &= false;
+      newErr.passwordConfirm = true;
+    }else{
+      newErr.passwordConfirm = false;
+    }
+    if(position === ''){
+      check &= false;
+      newErr.position = true;
+    }else{
+      newErr.position = false;
+    }
+    if(!image){
+      check &= false;
+      newErr.image = true
+    }else{
+      newErr.image = false;
+    }
+
+    setErr(newErr);
+    return check;
+  }
+  const submit = async() => {
+    console.log('check: ', vadlidateData());
+  }
+
   return (
     <Container component="main" maxWidth="sm">
       <CssBaseline />
@@ -112,6 +202,7 @@ export default function SignIn() {
             name="name"
             autoComplete="Your name"
             autoFocus
+            error={err.name}
           />
           <TextField
             variant="outlined"
@@ -122,9 +213,16 @@ export default function SignIn() {
             name="code"
             autoComplete="Your code"
             autoFocus
+            error={err.code}
           />
-
+          <Alert 
+            severity="error"
+            className={classnames({"hidden": !(err.name || err.code)})}
+          >
+            Name and code is require, use 5 characters or more for your code. Example for code: ct123
+          </Alert>
           <TextField
+            error="true"
             variant="outlined"
             margin="normal"
             required
@@ -134,8 +232,14 @@ export default function SignIn() {
             name="usernmae"
             autoComplete="Username"
             autoFocus
+            error={err.username}
           />
-
+          <Alert 
+            severity="error"
+            className={classnames({"hidden": !err.username})}
+          >
+            Username is require, use 8 characters or more for your username
+          </Alert>
           <TextField
             variant="outlined"
             margin="normal"
@@ -146,7 +250,14 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            error={err.password}
           />
+          <Alert 
+            severity="error"
+            className={classnames({"hidden": !err.password})}  
+          >
+            Password is require, use 8 characters or more for your password
+          </Alert>
           <TextField
             variant="outlined"
             margin="normal"
@@ -155,24 +266,33 @@ export default function SignIn() {
             name="passwordconfirm"
             label="Confirm Password"
             type="password"
-            id="passwordconfirm"
+            id="passwordConfirm"
             autoComplete="current-password"
+            error={err.passwordConfirm}
           />
+          <Alert 
+            id="passwordConfirm"
+            severity="error"
+            className={classnames({"hidden": !err.passwordConfirm})}  
+          >
+            Those passwords didn’t match. Try again.
+          </Alert>
         <InputLabel 
           className={classes.spaceingAndWith} 
-          id="demo-controlled-open-select-label">
+          id="demo-controlled-open-select-label"
+          >
             Position
         </InputLabel>
         <Select
-          
           labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
+          id="position"
           open={open}
           onClose={handleClose}
           onOpen={handleOpen}
           value={position}
           onChange={handleChange}
           className={classes.mediumWidth}
+          error={err.position}
         >
           <MenuItem value="">
             <em>None</em>
@@ -182,22 +302,33 @@ export default function SignIn() {
         </Select>
 
         <input 
-          className={classes.hiddenIuputImg} 
+          className="hidden" 
           type="file" 
           accept="image/x-png,image/gif,image/jpeg"
-          id="inputImg"
+          id="image"
+          onChange={getImage}
           />
         <Button
             onClick={handleInput}
-            type="submit"
             variant="contained"
             color="primary"
             className={classes.marginLeft}
           >
             Image
           </Button>
+          <Avatar 
+            alt="Avatar" 
+            src={srcImage}
+            className="reviewImg"
+          />
+          <Alert 
+            severity="error" 
+            className={classnames({"hidden": !err.image})}
+          >
+            Image is require
+          </Alert>
           <Button
-            type="submit"
+            onClick={submit}
             fullWidth
             variant="contained"
             color="primary"
